d02dfc05552ee4578c18d9a8b3a2d250ecfb9c89
Vadim Venediktov <install.vv@gmail.com>
2012-04-02 18:22:00 +0400

    Fix: Creation of a numbers with variable short aliases fixed

--- add_phone.sh
+++ add_phone.sh
@@ -17,7 +17,7 @@ else
 
   curl --cookie cookie.txt "$HTTP_PATH/get_contacts/?start=0&limit=9999&rel_id=$GROUP_ID&rnd=0.$RANDOM" > index.html
 
-  PHONE_ID=$(cat index.html | sed 's/.*{"id":"\([0-9]*\)","name":".*","user_id":"","short_phone":"8002"}.*/\1/g')
+  PHONE_ID=$(cat index.html | sed "s/.*{\"id\":\"\([0-9]*\)\",\"name\":\".*\",\"user_id\":\"\",\"short_phone\":\"$SHORT\"}.*/\1/g")
 
   PARAMS="protocol_id=1&phone=$PHONE&contact_id=$PHONE_ID"
 
